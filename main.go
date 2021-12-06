5b5820b7791df514bc4145ec415766a555aab2e8
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2021-12-06 16:41:01 -0300

    feat: more customization options, graceful shutdown (#1)
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

--- main.go
+++ main.go
@@ -2,7 +2,12 @@
 package promwish
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/charmbracelet/wish"
@@ -13,28 +18,37 @@ import (
 )
 
 // Middleware starts a HTTP server on the given address, serving the metrics from the default registerer to /metrics.
-func Middleware(address string) wish.Middleware {
-	http.Handle("/metrics", promhttp.Handler())
-	go http.ListenAndServe(address, nil) // TODO: should probably better handle server shutdown
-	return MiddlewareRegistry(prometheus.DefaultRegisterer)
+func Middleware(address, app string) wish.Middleware {
+	go func() {
+		Listen(address)
+	}()
+	return MiddlewareRegistry(
+		prometheus.DefaultRegisterer,
+		prometheus.Labels{
+			"app": app,
+		},
+	)
 }
 
 // Middleware setup the metrics for the given registry without starting any HTTP servers.
 // The caller is then responsible for serving the metrics.
-func MiddlewareRegistry(registry prometheus.Registerer) wish.Middleware {
+func MiddlewareRegistry(registry prometheus.Registerer, constLabels prometheus.Labels) wish.Middleware {
 	sessionsCreated := promauto.With(registry).NewCounter(prometheus.CounterOpts{
-		Name: "wish_sessions_created_total",
-		Help: "The total number of sessions created",
+		Name:        "wish_sessions_created_total",
+		Help:        "The total number of sessions created",
+		ConstLabels: constLabels,
 	})
 
 	sessionsFinished := promauto.With(registry).NewCounter(prometheus.CounterOpts{
-		Name: "wish_sessions_finished_total",
-		Help: "The total number of sessions created",
+		Name:        "wish_sessions_finished_total",
+		Help:        "The total number of sessions created",
+		ConstLabels: constLabels,
 	})
 
 	sessionsDuration := promauto.With(registry).NewCounter(prometheus.CounterOpts{
-		Name: "wish_sessions_duration_seconds",
-		Help: "The total sessions duration in seconds",
+		Name:        "wish_sessions_duration_seconds",
+		Help:        "The total sessions duration in seconds",
+		ConstLabels: constLabels,
 	})
 
 	return func(sh ssh.Handler) ssh.Handler {
@@ -48,4 +62,33 @@ func MiddlewareRegistry(registry prometheus.Registerer) wish.Middleware {
 			sh(s)
 		}
 	}
+}
+
+// Listen starts a HTTP server on the given address, serving the metrics from the default registerer to /metrics.
+// It handles exit signals to gracefully shutdown the server.
+func Listen(address string) {
+	srv := &http.Server{
+		Addr:    address,
+		Handler: promhttp.Handler(),
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to start metrics server: %s\n", err)
+		}
+	}()
+	log.Printf("Serving metrics at http://%s/metrics", address)
+
+	<-done
+	log.Print("Stopping metrics server")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() { cancel() }()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Failed to shutdown metrics server: %+v", err)
+	}
+	log.Print("Shutdown metrics server")
 }
